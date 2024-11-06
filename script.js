8ebdd3406400e60660645c56c84cef84e860f072
Yasin <yasinabdi.amir@gmail.com>
2024-11-06 08:08:27 -0500

    finshed SignUp

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 function signup(){ 
     let email = document.getElementById("email").value;
-    let pass = document.getElementById("pass").value;
+    let pass = document.getElementById("password").value;
     //console.log(email);
     //console.log(pass);
 
     localStorage.setItem(email, pass);
+    if(localStorage.getItem(email)) {
+        if(pass == localStorage.getItem(email)){
+            location.replace("home.html");
+        }
+        else{
+            alert("Incorrect");
+
+        }
+
+    }
+    else{
+        alert("User does not exist");
+    }
 }
+
+
+
 
 function login(){
     let email = document.getElementById("email").value;
-    let pass = document.getElementById("pass").value;
+    let pass = document.getElementById("password").value;
     //console.log(email);
     //console.log(pass);
 
