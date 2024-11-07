b01a05216aa0af92caf376e7059e26551cd4aed7
Airook <181858985+Airook-Sanjin@users.noreply.github.com>
2024-11-07 11:30:08 -0500

    Attempt to make the popup work on every page

--- script.js
+++ script.js
@@ -36,14 +36,49 @@ function initializeSignInPage() {
   document.getElementById("Signup-btn").addEventListener("click", signup);
   document.getElementById("Login-btn").addEventListener("click", login);
 }
-function initializeMainPage() {}
+function initializeMainPage() {
+  //  -----------------------------------------------------------------------
+  //                                Modal Boxes
+  //  -----------------------------------------------------------------------
+  let MainmodalBtn = document.getElementsByClassName("Modalbutton Main")[0];
+  
+  let Mainmodal = document.getElementsByClassName("modal Main")[0];
+  let Mainclosebtn = document.getElementsByClassName("Closebtn Main")[0];
+  MainmodalBtn.addEventListener("click", function () {
+    console.log(MainmodalBtn);
+    Mainmodal.style.display = "block";
+  });
+  Mainclosebtn.addEventListener("click", function () {
+    Mainmodal.style.display = "none";
+  });
+  window.onclick = function (event) {
+    if (event.target == Mainmodal) {
+      Mainmodal.style.display = "none";
+    }
+  };
+  let logo = document.getElementById("logo");
+  logo.addEventListener("click", function () {
+    location.replace("index.html");
+  });
+  let mainDeliverybtn = document.getElementsByClassName("Delivery-Main")[0];
+  let mainPickupbtn = document.getElementsByClassName("Pickup-Main")[0];
+
+  let maindeliveryForm =
+    document.getElementsByClassName("Main-in-container")[0];
+  mainDeliverybtn.addEventListener("click", function () {
+    maindeliveryForm.style.display = "block";
+  });
+  mainPickupbtn.addEventListener("click", function () {
+    maindeliveryForm.style.display = "none";
+  });
+}
 function initializeMenuPage() {
   //  -----------------------------------------------------------------------
   //                                Modal Boxes
   //  -----------------------------------------------------------------------
-  let modalBtn = document.getElementById("Modalbutton");
-  let modal = document.getElementById("Mymodal");
-  let closebtn = document.getElementById("Closebtn");
+  let modalBtn = document.getElementsByClassName("Modalbutton Menu")[0];
+  let modal = document.getElementsByClassName("modal Menu")[0];
+  let closebtn = document.getElementsByClassName("Closebtn Menu")[0];
   modalBtn.addEventListener("click", function () {
     modal.style.display = "block";
   });
@@ -55,19 +90,23 @@ function initializeMenuPage() {
       modal.style.display = "none";
     }
   };
-let logo = document.getElementById("logo");
-logo.addEventListener("click", function(){
+  let logo = document.getElementById("logo");
+  logo.addEventListener("click", function () {
     location.replace("index.html");
-});
-let Deliverybtn = document.getElementById("Deliverybtn");
-let Pickupbtn = document.getElementById("Pickupbtn");
-let indicator = document.getElementById("D-P-Indicator");
-Deliverybtn.addEventListener("click", function(){
-indicator.innerHTML=`<ion-icon name="pin"></ion-icon> Delivery`
-Pickupbtn.addEventListener("click", function(){
-    indicator.innerHTML = `<ion-icon name="pin"></ion-icon> Pick Up`
-})
-})
+  });
+  let Deliverybtn = document.getElementsByClassName("Delivery-Menu")[0];
+  let Pickupbtn = document.getElementsByClassName("Pickup-Menu")[0];
+  let indicator = document.getElementById("D-P-Indicator");
+  let deliveryForm = document.getElementsByClassName("in-container")[0];
+  Deliverybtn.addEventListener("click", function () {
+    indicator.innerHTML = `<ion-icon name="pin"></ion-icon> Delivery`;
+    deliveryForm.style.display = "block";
+  });
+  Pickupbtn.addEventListener("click", function () {
+    indicator.innerHTML = `<ion-icon name="pin"></ion-icon> Pick Up`;
+    deliveryForm.style.display = "none";
+  });
+
   //  -----------------------------------------------------------------------
   //                                Modal Boxes
   //  -----------------------------------------------------------------------
