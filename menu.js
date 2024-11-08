c416ce6f0d290b943a48009c756691d84b10787e
emi-co <colegioenc@gmail.com>
2024-11-08 10:34:45 -0500

    Updated Styles

---
+++ menu.js
@@ -0,0 +1,16 @@
+let topButton = document.getElementById("topBtn")
+window.onscroll = function() {scrollFunction()};
+
+
+function scrollFunction(){
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) 
+{  topButton.style.display = "block";
+
+} else { 
+    topButton.style.display = "none";
+}
+}
+
+function backToTop () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
