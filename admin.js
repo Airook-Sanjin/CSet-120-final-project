2255ffd90be1bf01aea0f323af21a6c1883bd380
Yasin <yasinabdi.amir@gmail.com>
2024-11-13 09:39:22 -0500

    changes

--- admin.js
+++ admin.js
@@ -4,28 +4,3 @@ document.querySelectorAll(".item-card .close-button").forEach(button => {
     }); 
 });
 
-const addItemForm = document.getElementById("add-item-form");
-const addItemButton = document.getElementById("add-item-button");
-const menu = document.getElementById("menu");
-
-addItemButton.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    const itemName = document.getElementById("item-name").value;
-    const itemDescription = document.getElementById("item-description").value;
-    const itemPrice = document.getElementById("item-price").value;
- 
-
-    const newItem = document.createElement("div");
-    newItem.innerHTML = `
-    <h2>${itemName}</h2>
-    <p>${itemDescription}</p>
-    <p>Price: $${itemPrice}</p>
-
-    `;
-
-
-menu.appendChild(newItem);
-
-});
-
