e673bac7dc7d254c4a1d30ea0704519b64c2c82b
Yasin <yasinabdi.amir@gmail.com>
2024-12-05 10:08:11 -0500

    stuff

---
+++ menu.js
@@ -0,0 +1,17 @@
+let topButton = document.getElementById('topBtn');
+// let menuNav = document.getElementsByClassName('menu-nav')
+window.onscroll = function () {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		topButton.style.display = 'block';
+	} else {
+		topButton.style.display = 'none';
+	}
+}
+
+function backToTop() {
+	window.scrollTo({ top: 0, behavior: 'smooth' });
+}
---
+++ progress.js
@@ -0,0 +1,44 @@
+/* -------------------------------------------------------------------------- */
+/*                                Progress Bar                                */
+/* -------------------------------------------------------------------------- */
+const steps = [
+    document.getElementById('received'),
+    document.getElementById('preparation'),
+    document.getElementById('delivery'),
+    document.getElementById('delivered')
+];
+
+let currentStep = 0; 
+
+function updateProgressBar() {
+    if (currentStep < steps.length) {
+        steps[currentStep].classList.add('active');
+        if (currentStep > 0) {
+            steps[currentStep - 1].classList.add('completed');
+        }
+        currentStep++;
+    }
+}
+
+
+setTimeout(updateProgressBar, 1000);
+setTimeout(updateProgressBar, 4000); 
+setTimeout(updateProgressBar, 7000); 
+setTimeout(updateProgressBar, 10000); 
+
+
+/* -------------------------------------------------------------------------- */
+/*                                  Date/Time                                 */
+/* -------------------------------------------------------------------------- */
+  function updateDateTime() {
+    const currentDate = new Date();
+    
+
+    const date = currentDate.toLocaleDateString();  
+    const time = currentDate.toLocaleTimeString();  
+
+    document.getElementById('order-date').textContent = date;
+    document.getElementById('order-time').textContent = time;
+}
+
+updateDateTime();
