0fec9c71de6f38cea02e5389499b38391c605765
Yasin <yasinabdi.amir@gmail.com>
2024-12-07 12:13:59 -0500

    changes

--- script.js
+++ script.js
@@ -360,6 +360,8 @@ function menuDisplay() {
 }
 
 function initializeSignInPage() {
+  
+  
   function signup() {
     let email = document.getElementById("email").value;
     let pass = document.getElementById("password").value;
